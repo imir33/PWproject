112cd33a7d03f267488e6c6f46b020fd00c2695f
Ionut Iacob <ionut@Ubuntu20.04>
2020-10-15 19:50:10 +0300

    Added Add New Book route

--- routes/api/books.js
+++ routes/api/books.js
@@ -25,4 +25,46 @@ router.get('/', auth, async (req, res) => {
   }
 });
 
+// @route    POST api/books
+// @desc     Add a new book entry
+// @access   Private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('title', 'Title is required').not().isEmpty(),
+      check('author', 'Author is required').not().isEmpty(),
+      check('numberOfPages', 'Number of pages is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+
+      const { title, author, numberOfPages, currentPage } = req.body;
+
+      const newBook = new Book({
+        user: req.user.id,
+        title: title,
+        author: author,
+        numberOfPages: Number.parseInt(numberOfPages),
+        currentPage: Number.parseInt(currentPage),
+      });
+
+      const book = await newBook.save();
+
+      res.json(book);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
 module.exports = router;
