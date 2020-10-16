7ac585bdf89e5565bca7b9f55e773391fa130e6e
Ionut Iacob <ionut@Ubuntu20.04>
2020-10-16 22:33:22 +0300

    Added Get Friends of current User route

--- routes/api/friends.js
+++ routes/api/friends.js
@@ -1,9 +1,27 @@
+const e = require('express');
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const User = require('../../models/User');
 
 // @route    GET api/friends
-// @desc     Test Route
-// @access   Public
-router.get('/', (req, res) => res.send('Friends Route'));
+// @desc     Get all friends of current user
+// @access   Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+
+    if (user.friends.length === 0) {
+      res.json({ msg: 'No friends found for this user' });
+    } else {
+      res.send(user.friends);
+    }
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
