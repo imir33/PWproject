7a7beefedc927a12b285034de331b3d949c0cca8
Ionut Iacob <ionut@Ubuntu20.04>
2020-10-21 15:39:11 +0300

    Added react links for the login and register pages

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
-    <section className="landing">
-      <div className="dark-overlay">
-        <div className="landing-inner">
-          <h1 className="x-large">Reading Tracker</h1>
-          <p className="lead">Track your reading progress and share it with your friends</p>
-          <div className="buttons">
-            <a href="register.html" className="btn btn-primary">Sign up</a>
-            <a href="login.html" className="btn">Login</a>
+    <section className='landing'>
+      <div className='dark-overlay'>
+        <div className='landing-inner'>
+          <h1 className='x-large'>Reading Tracker</h1>
+          <p className='lead'>Track your reading progress and share it with your friends</p>
+          <div className='buttons'>
+            <Link to='/register' className='btn btn-primary'>Sign up</Link>
+            <Link to='/login' className='btn'>Login</Link>
           </div>
         </div>
       </div>
