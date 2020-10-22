0b6bc6b56855c536bedeba9b869164f1f7a49e57
Ionut Iacob <ionut@Ubuntu20.04>
2020-10-22 12:01:57 +0300

    Modified the register action

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,26 +1,24 @@
 //import api from '../utils/api';
 import axios from 'axios';
 import { setAlert } from './alert';
-import {
-  REGISTER_SUCCESS,
-  REGISTER_FAIL,
-  USER_LOADED,
-  AUTH_ERROR,
-} from './types';
+import { REGISTER_SUCCESS, REGISTER_FAIL } from './types';
 
 // Register User
-export const register = (formData) => async (dispatch) => {
+export const register = ({ name, email, password }) => async (dispatch) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  const body = JSON.stringify({ name, email, password });
   try {
-    const res = await axios.post('api/users', formData);
+    const res = await axios.post('api/users', body, config);
 
     dispatch({
       type: REGISTER_SUCCESS,
       payload: res.data,
     });
-    //dispatch(loadUser());
   } catch (err) {
-    // console log this
-    console.log(err);
     const errors = err.response.data.errors;
 
     if (errors) {
