a5a47480e2d4e7e6075b16e5da1725ce9a9032c0
Ionut Iacob <ionut@Ubuntu20.04>
2020-10-22 13:01:09 +0300

    Added token based registration with token stored locally

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,7 +1,8 @@
 import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
-  REMOVE_ALERT,
+  USER_LOADED,
+  AUTH_ERROR,
 } from '../actions/types';
 
 const initialState = {
@@ -15,6 +16,13 @@ export default function (state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload,
+      };
     case REGISTER_SUCCESS:
       localStorage.setItem('token', payload.token);
       return {
@@ -24,6 +32,7 @@ export default function (state = initialState, action) {
         loading: false,
       };
     case REGISTER_FAIL:
+    case AUTH_ERROR:
       localStorage.removeItem('token');
       return {
         ...state,
